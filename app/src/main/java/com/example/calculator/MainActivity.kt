2c0424d8ceab37d0f1b65b8c55c74979a699e875
Emerson Silva <emerson_jdss@hotmail.com>
2020-12-10 08:53:27 -0300

    config buttons

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.calculator
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -9,51 +10,51 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 //
-////    //btn numbers
-   lateinit var btOne: Button
-   lateinit var btTwo: Button
-   lateinit var btThree: Button
-   lateinit var btFour: Button
-   lateinit var btFive: Button
-   lateinit var btSix: Button
-   lateinit var btSeven: Button
-   lateinit var btEight: Button
-   lateinit var btNine: Button
-   lateinit var btZero: Button
+// //    //btn numbers
+   private lateinit var btOne: Button
+   private lateinit var btTwo: Button
+   private lateinit var btThree: Button
+   private lateinit var btFour: Button
+   private lateinit var btFive: Button
+   private lateinit var btSix: Button
+   private lateinit var btSeven: Button
+   private lateinit var btEight: Button
+   private lateinit var btNine: Button
+   private lateinit var btZero: Button
 
 //    btn operations
-   lateinit var btDot: Button
-   lateinit var btPi: Button
-   lateinit var btEqual: Button
-   lateinit var btPlus: Button
-   lateinit var btMin: Button
-   lateinit var btDiv: Button
-   lateinit var btMul: Button
-   lateinit var btInv: Button
-   lateinit var btSqrt: Button
-   lateinit var btSquare: Button
-   lateinit var btFat: Button
-   lateinit var btLn: Button
-   lateinit var btCos: Button
-   lateinit var btTan: Button
-   lateinit var bt1: Button
-   lateinit var bt2: Button
-   lateinit var btC: Button
-   lateinit var btAc: Button
-   lateinit var btLog: Button
-   lateinit var btSin: Button
+   private lateinit var btDot: Button
+   private lateinit var btPi: Button
+   private lateinit var btEqual: Button
+   private lateinit var btPlus: Button
+   private lateinit var btMin: Button
+   private lateinit var btDiv: Button
+   private lateinit var btMul: Button
+   private lateinit var btInv: Button
+   private lateinit var btSqrt: Button
+   private lateinit var btSquare: Button
+   private lateinit var btFat: Button
+   private lateinit var btLn: Button
+   private lateinit var btCos: Button
+   private lateinit var btTan: Button
+   private lateinit var bt1: Button
+   private lateinit var bt2: Button
+   private lateinit var btC: Button
+   private lateinit var btAc: Button
+   private lateinit var btLog: Button
+   private lateinit var btSin: Button
 
-   lateinit var tvMain: TextView
-   lateinit var tvSec: TextView
+   private lateinit var tvMain: TextView
+   private lateinit var tvSec: TextView
 
    val pi: String = "3.14159265"
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//
-////        operators
+      // operators
        bt1 = findViewById(R.id.bt_1)
        bt2 = findViewById(R.id.bt_2)
 
@@ -65,7 +66,7 @@ class MainActivity : AppCompatActivity() {
        btDiv = findViewById(R.id.bt_div)
        btInv = findViewById(R.id.bt_inv)
        btSqrt = findViewById(R.id.bt_sqrt)
-       btSquare = findViewById(R.id.square)
+       btSquare = findViewById(R.id.bt_square)
        btFat = findViewById(R.id.bt_fat)
        btLn = findViewById(R.id.bt_ln)
        btLog = findViewById(R.id.bt_log)
@@ -74,12 +75,10 @@ class MainActivity : AppCompatActivity() {
        btTan = findViewById(R.id.bt_tan)
        btDot = findViewById(R.id.bt_dot)
 
-
        btAc = findViewById(R.id.bt_ac)
        btC = findViewById(R.id.bt_c)
 
-
-       numbers
+      //  numbers
        btOne = findViewById(R.id.bt_one)
        btTwo = findViewById(R.id.bt_two)
        btThree = findViewById(R.id.bt_3)
@@ -91,15 +90,60 @@ class MainActivity : AppCompatActivity() {
        btNine = findViewById(R.id.bt_9)
        btZero = findViewById(R.id.bt_0)
 
-
        tvMain = findViewById(R.id.tvmain)
        tvSec = findViewById(R.id.tvsec)
 
+//       btOne.setOnClickListener(View.OnClickListener { tvMain.text = tvMain.text.toString() + "1" })
 
-       btOne.setOnClickListener(View.OnClickListener { tvMain.text = tvMain.text.toString() + "1" })
+       btOne.setOnClickListener(View.OnClickListener {
+          val value: String = tvMain.text.toString()
+          tvMain.text = value + btOne.text.toString()
+       })
+
+       btTwo.setOnClickListener(View.OnClickListener {
+          val value: String = tvMain.text.toString()
+          tvMain.text = value + btTwo.text.toString()
+       })
+
+       btThree.setOnClickListener(View.OnClickListener {
+          val value: String = tvMain.text.toString()
+          tvMain.text = value + btThree.text.toString()
+       })
+
+       btFour.setOnClickListener(View.OnClickListener {
+          val value: String = tvMain.text.toString()
+          tvMain.text = value + btFour.text.toString()
+       })
+
+       btFive.setOnClickListener(View.OnClickListener {
+          val value: String = tvMain.text.toString()
+          tvMain.text = value + btFive.text.toString()
+       })
+
+       btSix.setOnClickListener(View.OnClickListener {
+          val value: String = tvMain.text.toString()
+          tvMain.text = value + btSix.text.toString()
+       })
+
+       btSeven.setOnClickListener(View.OnClickListener {
+          val value: String = tvMain.text.toString()
+          tvMain.text = value + btSeven.text.toString()
+       })
+
+       btEight.setOnClickListener(View.OnClickListener {
+          val value: String = tvMain.text.toString()
+          tvMain.text = value + btEight.text.toString()
+       })
+
+       btNine.setOnClickListener(View.OnClickListener {
+          val value: String = tvMain.text.toString()
+          tvMain.text = value + btNine.text.toString()
+       })
+
+       btZero.setOnClickListener(View.OnClickListener {
+          val value: String = tvMain.text.toString()
+          tvMain.text = value + btZero.text.toString()
+       })
+
     }
-
-
-
-
 }
